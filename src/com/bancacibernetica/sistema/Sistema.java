d0258cb3e661eb5c78cc8daf58e4e3d8386ed6e3
Manuel Ciriaco <eldjemi@hotmail.com>
2013-06-17 16:37:30 -0800

    Clase sistema creada

---
+++ src/com/bancacibernetica/sistema/Sistema.java
@@ -0,0 +1,13 @@
+package com.bancacibernetica.sistema;
+
+public class Sistema {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
